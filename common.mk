a4e22a0104f8e757193317225c5eaf9400b02b76
Jackeagle <jackeagle102@gmail.com>
2022-10-26 11:51:01 +0200

    sm8150-common: Build rmnet control library
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common.mk
+++ common.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
 
+# Data
+PRODUCT_PACKAGES += \
+    librmnetctl
+    
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
