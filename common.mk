a99fe6b0977553434bd2bb3e379892e19a4e1fce
Jackeagle <jackeagle102@gmail.com>
2023-01-08 13:15:48 -0500

    sm8150-common: init: Drop init.qti.qcv.sh
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common.mk
+++ common.mk
@@ -192,7 +192,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
     init.qti.dcvs.sh \
-    init.qti.qcv.sh \
     init.qcom.factory.rc \
     init.qcom.rc \
     init.set_kptr.rc \
