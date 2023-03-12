955af688c1003e935dffca907139304a8e5c1f56
‘brian-tango <3495618701@qq.com>
2023-03-12 12:46:53 +0800

    sm8150-common: Configure architecture and platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,5 +8,32 @@ BOARD_VENDOR := meizu
 
 COMMON_PATH := device/meizu/sm8150-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msmnile
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msmnile
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno640
+
+
 # Inherit from the proprietary version
 -include vendor/meizu/sm8150-common/BoardConfigVendor.mk
