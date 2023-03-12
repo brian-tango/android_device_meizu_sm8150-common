516529878e749aadbd65a8145b7cacc47c6ff99b
‘brian-tango <3495618701@qq.com>
2023-03-12 17:04:19 +0800

    sm8150-common: Build TextClassifier

--- common.mk
+++ common.mk
@@ -381,6 +381,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
